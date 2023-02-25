bb4b47d05b08119ef4ede9d8327344a533e4ef2d
ashans <ashan.shamika@gmail.com>
2023-02-25 12:28:13 +0530

    Add PWA support

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 import {persistor, store} from "./store/store";
 import {PersistGate} from "redux-persist/integration/react";
 import {Elements} from "@stripe/react-stripe-js";
 import {stripePromise} from "./utils/stripe/stripe.util";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -29,3 +29,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
